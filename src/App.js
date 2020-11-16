3e4ff14736aa4d852b6bf2b86bb1394371e5d571
Vicky Kumar <anuranjanranawat.1@gmail.com>
2020-11-16 13:19:44 +0530

    added bar chart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
-import image from './images/image.png';
-import Card from './Components/Card'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import image from "./images/image.png";
+import Card from "./Components/Card";
+import blackLine from "./images/black.svg";
+import blueLine from "./images/blue.svg";
+import redLine from "./images/red.svg";
+import greenLine from "./images/green.svg";
+import { Bar } from "react-chartjs-2";
 
 // active": "10879",
 // "confirmed": "211443",
@@ -17,53 +22,114 @@ import Card from './Components/Card'
 // "state": "Bihar",
 // "statecode": "BR",
 // "statenotes"
-const App =  () => {
+const App = () => {
+  const [active, setActive] = useState("");
+  const [confirmed, setConfirmed] = useState("");
+  const [deaths, setDeaths] = useState("");
+  const [recovered, setRecovered] = useState("");
+  const [deltaconfirmed, setDeltaconfirmed] = useState("");
+  const [deltadeaths, setDeltadeaths] = useState("");
+  const [deltarecovered, setDeltaRecovered] = useState("");
+  const [state, setState] = useState("");
+  const [updateddate, setLastupdateddate] = useState("");
+  // const [lastupdatedtime, setLastupdatedtime] = useState("");
 
-    const[active, setActive] = useState("");
-    const[confirmed, setConfirmed] = useState("");
-    const[deaths, setDeaths] = useState("");
-    const[recovered, setRecovered] = useState("");
-    const[deltaconfirmed, setDeltaconfirmed] = useState("");
-    const[deltadeaths, setDeltadeaths] = useState("");
-    const[deltarecovered, setDeltaRecovered] = useState("");
-    const[state, setState] = useState("");
-    const[updateddate, setLastupdateddate] = useState("");
-    const[lastupdatedtime, setLastupdatedtime] = useState("");
+  // const datas = [confirmed, deaths, recovered];
+  // const deltaData = [deltaconfirmed, deltadeaths, deltarecovered];
+  // const cardName = ["Confirmed", "Deaths", "Recovered"];
 
-    // const datas = [confirmed, deaths, recovered];
-    // const deltaData = [deltaconfirmed, deltadeaths, deltarecovered];
-    // const cardName = ["Confirmed", "Deaths", "Recovered"];
+  const apiURL = "https://api.covid19india.org/data.json";
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const {
+      data: { statewise },
+    } = await axios.get(apiURL);
+    setActive(statewise[11].active);
+    setConfirmed(statewise[11].confirmed);
+    setDeaths(statewise[11].deaths);
+    setRecovered(statewise[11].recovered);
+    setDeltaconfirmed(statewise[11].deltaconfirmed);
+    setDeltadeaths(statewise[11].deltadeaths);
+    setDeltaRecovered(statewise[11].deltarecovered);
+    setState(statewise[11].state);
+    setLastupdateddate(statewise[11].lastupdatedtime.slice(0, 10));
+  };
 
-    const apiURL = "https://api.covid19india.org/data.json";
-    useEffect(() =>{
-        fetchData();
-    },[]);
-    const fetchData = async() =>{
-        const {data:{ statewise } } = await axios.get(apiURL);
-        setActive(statewise[11].active);
-        setConfirmed(statewise[11].confirmed);
-        setDeaths(statewise[11].deaths);
-        setRecovered(statewise[11].recovered);
-        setDeltaconfirmed(statewise[11].deltaconfirmed);
-        setDeltadeaths(statewise[11].deltadeaths);
-        setDeltaRecovered(statewise[11].deltarecovered);
-        setState(statewise[11].state);
-        setLastupdateddate(statewise[11].lastupdatedtime.slice(0,10));
-    }
-
-    return ( 
-          <div class="container">
-          <img src={image} alt="COVID-19" style={{padding: 20}}/>
-          <h1>{state}</h1>
-          <div class= "secondContainer">
-          <Card name ={"Active"} value ={active} updateTime = {updateddate}/>
-          <Card name ={"Confirmed"} value ={confirmed} value2={deltaconfirmed} updateTime = {updateddate}/>
-          <Card name ={"Recovered"} value ={recovered} value2={deltarecovered} updateTime = {updateddate}/>
-          <Card name ={"Deaths"} value ={deaths} value2={deltadeaths} updateTime = {updateddate}/>
-          
-          </div>  
-          </div>
-      );
-}
+  return (
+    <div class='container'>
+      <img src={image} alt='COVID-19' style={{ padding: 20 }} />
+      <h1>{state}</h1>
+      <div class='secondContainer'>
+        <Card
+          style={{
+            backgroundColor: "rgba(0, 0, 255, 0.2)",
+            color: "rgba(0, 0, 255)",
+          }}
+          name={"Confirmed"}
+          value={confirmed}
+          value2={deltaconfirmed}
+          updateTime={updateddate}
+          img={blueLine}
+        />
+        <Card
+          style={{
+            backgroundColor: "rgba(255, 0, 0, 0.2)",
+            color: "rgba(255, 0, 0)",
+          }}
+          name={"Active"}
+          value={active}
+          updateTime={updateddate}
+          img={redLine}
+        />
+        <Card
+          style={{
+            backgroundColor: "rgba(0, 255, 0, 0.2)",
+            color: "rgba(0, 255, 0)",
+          }}
+          name={"Recovered"}
+          value={recovered}
+          value2={deltarecovered}
+          updateTime={updateddate}
+          img={greenLine}
+        />
+        <Card
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+          name={"Deaths"}
+          value={deaths}
+          value2={deltadeaths}
+          updateTime={updateddate}
+          img={blackLine}
+        />
+      </div>
+      <div className='chart'>
+        <Bar
+          data={{
+            labels: ["Confirmed", "Active", "Recovered", "Deaths"],
+            datasets: [
+              {
+                barPercentage: 0.5,
+                minBarLength: 2,
+                label: "People",
+                backgroundColor: [
+                  "rgba(0, 0, 255, 0.5)",
+                  "rgba(255, 0, 0, 0.5)",
+                  "rgba(0, 255, 0, 0.5)",
+                  "rgba(0, 0, 0, 0.5)",
+                ],
+                data: [confirmed, active, recovered, deaths],
+              },
+            ],
+          }}
+          options={{
+            legend: { display: false },
+            title: { display: true, text: "Bihar" },
+          }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import Countup from 'react-countup';
+import React from "react";
+import Countup from "react-countup";
 import "./Card.css";
 
 const Card = (props) => {
-    return (
-        <div class="card">
-            <h1>{props.name}</h1>
-            <div class="innercontainer">
-            <h2><Countup end={props.value} duration={2} separator=','/></h2>
-            <h3><Countup end = {props.value2}  duration={2} separator=','/></h3>
-            <h4>{props.updateTime}</h4>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div class='card' style={props.style}>
+      <div className='detail'>
+        <h2>{props.name}</h2>
+        <h2>
+          <Countup end={props.value} duration={2} separator=',' />
+        </h2>
+        <h3>
+          <Countup end={props.value2} duration={2} separator=',' />
+        </h3>
+        <h4>{props.updateTime}</h4>
+      </div>
+      <img src={props.img} alt='lineChart' />
+    </div>
+  );
+};
 
-export default Card
+export default Card;
